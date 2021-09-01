413a94a4c44958f65361b35a2e190734e6cf192f
Nutthanon <35944443+nnakiyume@users.noreply.github.com>
2021-09-01 23:53:15 +0700

    Create Scene Handler

---
+++ GI498_Sages/Assets/_Scripts/EditorScript/SceneHandlerEditor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace _Scripts.EditorScript
+{
+    [CustomEditor(typeof(SceneHandler))]
+    public class SceneHandlerEditor  : Editor 
+    {
+        public override void OnInspectorGUI()
+        {
+            SceneHandler sceneHandler = (SceneHandler)target;
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("scenePackages"), true);
+            
+            sceneHandler.sceneName = EditorGUILayout.TextField("Scene Name", sceneHandler.sceneName);
+            
+            if(GUILayout.Button("Load Specific Scene"))
+            {
+                sceneHandler.LoadSpecificScene();
+            }
+            
+            if(GUILayout.Button("Unload Specific Scene"))
+            {
+                sceneHandler.UnloadSpecificScene();
+            }
+            
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            
+            if(GUILayout.Button("Load All Scene"))
+            {
+                sceneHandler.LoadAllScene();
+            }
+            
+            if(GUILayout.Button("Unload All Scene"))
+            {
+                sceneHandler.UnloadAllScene();
+            }
+            
+            
+        }
+    }
+}
---
+++ GI498_Sages/Assets/_Scripts/SceneHandler.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Scripts
+{
+    public class SceneHandler : MonoBehaviour
+    {
+        [Serializable]
+        struct ScenePackage
+        {
+            public bool needLoad;
+            public bool isLoaded;
+            public SceneAsset sceneAsset;
+        }
+    
+        [SerializeField] private List<ScenePackage> scenePackages;
+
+        [Header("Single Scene Handler")] 
+        public string sceneName;
+
+        public void LoadAllScene()
+        {
+            foreach (var scene in scenePackages)
+            {
+                if (scene.sceneAsset != null)
+                {
+                    var scenePackage = scene;
+                    if (scenePackage.isLoaded == false)
+                    {
+                        if (scenePackage.needLoad)
+                        {
+                            SceneManager.LoadScene(scenePackage.sceneAsset.name, LoadSceneMode.Additive);
+                            scenePackage.isLoaded = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void UnloadAllScene()
+        {
+            foreach (var scene in scenePackages)
+            {
+                if (scene.sceneAsset != null)
+                {
+                    var scenePackage = scene;
+                    if (scenePackage.isLoaded)
+                    {
+                        SceneManager.UnloadSceneAsync(scenePackage.sceneAsset.name);
+                        scenePackage.isLoaded = false;
+                    }
+                }
+            }
+        }
+
+        public void LoadSpecificScene()
+        {
+            if (sceneName != null || sceneName != "" || !sceneName.Contains(" "))
+            {
+                var index = GetScenePackageIndexByName(sceneName);
+                var scene = scenePackages[index];
+            
+                if (scene.sceneAsset != null)
+                {
+                    SceneManager.LoadSceneAsync(scene.sceneAsset.name, LoadSceneMode.Additive);
+                    scene.isLoaded = true;
+                }
+            }
+        }
+    
+        public void UnloadSpecificScene()
+        {
+            if (sceneName != null || sceneName != "" || !sceneName.Contains(" "))
+            {
+                var index = GetScenePackageIndexByName(sceneName);
+                var scene = scenePackages[index];
+            
+                if (scene.sceneAsset != null)
+                {
+                    SceneManager.UnloadSceneAsync(scene.sceneAsset.name);
+                    scene.isLoaded = false;
+                }
+            }
+        }
+
+        private int GetScenePackageIndexByName(string toGetSceneName)
+        {
+            return scenePackages.FindIndex(result => result.sceneAsset.name == toGetSceneName);
+        }
+    }
+}
