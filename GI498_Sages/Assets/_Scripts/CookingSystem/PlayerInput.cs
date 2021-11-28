c489881de3e058ea8e587423de2b3b437945b3ac
Nutthanon <35944443+nnakiyume@users.noreply.github.com>
2021-11-28 22:21:05 +0700

    Update Lib  2020.3.22f

--- GI498_Sages/Assets/_Scripts/CookingSystem/PlayerInput.cs
+++ GI498_Sages/Assets/_Scripts/CookingSystem/PlayerInput.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/_Scripts/Character/PlayerInput.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/_Scripts/CookingSystem/PlayerInput.inputactions'
 
 using System;
 using System.Collections;
