af0a271576fe11dba13547fec4025af458401999
Nutthanon <35944443+nnakiyume@users.noreply.github.com>
2021-12-11 11:20:24 +0700

    FixNameSpace

--- GI498_Sages/Assets/_Scripts/CookingSystem/IngredientInfoComponent.cs
+++ GI498_Sages/Assets/_Scripts/CookingSystem/IngredientInfoComponent.cs
@@ -1,18 +1,19 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class IngredientInfoComponent : MonoBehaviour
+namespace _Scripts.CookingSystem
 {
-    [SerializeField] private Image iconImage;
-    [SerializeField] private TMP_Text nameText;
-
-    public void InitComponent(Sprite image, String name)
+    public class IngredientInfoComponent : MonoBehaviour
     {
-        iconImage.sprite = image;
-        nameText.text = name;
+        [SerializeField] private Image iconImage;
+        [SerializeField] private TMP_Text nameText;
+
+        public void InitComponent(Sprite image, String name)
+        {
+            iconImage.sprite = image;
+            nameText.text = name;
+        }
     }
 }
